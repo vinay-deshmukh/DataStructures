c4766cad40b05e81ac22980ca551070cc029ef28
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2020-05-28 00:43:51 +0530

    Added Trie Tree Implementation in JS

---
+++ JavaScript/trie.js
@@ -0,0 +1,46 @@
+class TrieNode{
+    constructor(value){
+        this.children = {};
+        this.value = value;
+        this.is_end = false;
+    }
+}
+
+class Trie{
+    constructor(){
+        this.root = new TrieNode(null);
+    }
+
+    insert(s){
+        // Function to insert a string in a Trie Tree
+        var curr = this.root;
+        for(var i = 0; i < s.length; i++){
+            var char = s.charAt(i);
+            if(!(char in curr.children)){
+                curr.children[char] = new TrieNode(char);
+            }
+            curr = curr.children[char];
+        }
+        curr.is_end = true;
+    }
+
+    search(s, allow_prefix){
+        // Function to search whether a given string exists inside a Trie Tree
+        var curr = this.root;
+        for(var i = 0; i < s.length; i++){
+            var char = s.charAt(i);
+            if(!(char in curr.children)){
+                return false;
+            }
+            curr = curr.children[char];
+        }
+        return allow_prefix || curr.is_end;
+    }
+}
+
+// Sample test cases to test the Trie Tree.
+const trie = new Trie();
+trie.insert("abc");
+trie.insert("adc");
+console.log(trie.search("abc")); // True
+console.log(trie.search("cbv")); // False
